500f9fe15a63454264a8d0f74e2e0a39df252297
Fahri Can <fahri-can@gmx.at>
2016-11-19 13:13:31 +0100

    had to do a few changes

--- ISBN.java
+++ ISBN.java
@@ -5,51 +5,43 @@ import bwi.prog.utils.TextIO;
 public class ISBN {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String isbn;
+		boolean validPattern;
 		
 		TextIO.put("enter ISBN: ");
-		String isbn = TextIO.getln();
-		TextIO.putf("valid pattern: %s\n", validatePattern(isbn));
-		TextIO.putf("valid checkDigit: %s\n", validateCheckDigit(isbn));
-	}
-	
-	//validates "s" as a 13-digit number. 
-	// Returns true if "s" is a 13-digit number, false otherwise
-	static boolean validatePattern(String s){
-		
-		if(s.matches("[0-9]{13}")){
-			
-			return true;
+		isbn = TextIO.getln();
+		 
+		TextIO.putf("valid pattern: %b\n", validPattern = validatePattern(isbn));
+		TextIO.put("valid checkDigit: ");
+		if(validPattern) {
+			TextIO.putf("%b\n", validateCheckDigit(isbn));
 		}
 		else {
-			
-			return false;
+			TextIO.put("false\n");
 		}
 	}
 	
-	//validates "s" as an ISBN. Returns true if "s" is a 13-digit ISBN 
-	//(with correct check number), false otherwise.
-	static boolean validateCheckDigit(String s){
+	static boolean validatePattern(String s) {
+		return s.matches("\\d{13}");
+	}
+	
+	static boolean validateCheckDigit(String s) {
+		int last = Integer.parseInt(s.substring(s.length() - 1));
+		int h = 0;
 		
-		int lastDigit, sum = 0;
-		
-		lastDigit = Integer.parseInt(s.substring(12));
-		
-		
-		for(int i = 0; i < 12; i++) {
-			
+		for(int i = 0; i < s.length() - 1; i++) {
 			if(i != 0 && (i+1) % 2 == 0) {
-				sum += Integer.parseInt(s.substring(i, i+1)) * 3;
+				h += Integer.parseInt(s.substring(i, i+1)) * 3;
 			}
 			else {
-				
-				sum += Integer.parseInt(s.substring(i, i+1));
+				h += Integer.parseInt(s.substring(i, i+1));
 			}
 		}
 		
-		sum = ((10 - (sum) % 10) % 10);
+		h = ((10 - (h) % 10) % 10);
 		
-		if(sum == lastDigit) {
+		if(h == last) {
 			return true;
 		}
 		else {
@@ -57,6 +49,6 @@ public class ISBN {
 			return false;
 		}
 		
-		
 	}
+
 }
