0057e0967b6c6b466af8f5dd72558416ac87e63f
Fahri Can <fahri-can@gmx.at>
2016-12-11 18:01:44 +0100

    game logic

---
+++ OOP/Hangman/Game.java
@@ -0,0 +1,27 @@
+
+public class Game {
+
+	private String answer;
+	private String hits;
+	private String misses;
+		
+	public Game(String answer){
+		
+		this.answer = answer;
+		hits = "";
+		misses = "";
+	}
+	
+	public boolean applyGuesses(char letter) {
+		
+		boolean isHit = answer.indexOf(letter) != -1;
+		if (isHit) {
+			hits += letter;
+		}
+		else {
+			misses += letter;
+		}
+		
+		return isHit;
+	}
+}
