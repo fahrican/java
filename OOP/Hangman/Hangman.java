c5aeac3815cb9b4c9571b878723716ef703746bd
Fahri Can <fahri-can@gmx.at>
2016-12-13 13:56:07 +0100

    added objects

--- OOP/Hangman/Hangman.java
+++ OOP/Hangman/Hangman.java
@@ -5,6 +5,17 @@ public class Hangman {
 		// TODO Auto-generated method stub
 		
 		Game myGame = new Game("Monopoly");
+		Prompter prompter = new Prompter(myGame);
+		
+		prompter.displayProgress();
+		boolean isHit = prompter.promptForGuess();
+		if (isHit) {
+			System.out.println("You got a hit!");
+		}
+		else {
+			System.out.println("Ooops, that was not correct.");
+		}
+		prompter.displayProgress();
 	}
 
 }
