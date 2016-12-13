99ba645616ba76f4ce5c4cc647056307526ad702
Fahri Can <fahri-can@gmx.at>
2016-12-13 13:58:32 +0100

    addded method to ask user for input

--- OOP/Hangman/Prompter.java
+++ OOP/Hangman/Prompter.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Prompter {
-
-}
+	
+	private Game game;
+	
+	public Prompter(Game game){
+		
+		this.game = game;
+	}//end of constructor
+	
+	public boolean promptForGuess() {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a letter: ");
+		String guess = scanner.nextLine();
+		char letter = guess.charAt(0);
+		
+		return game.applyGuesses(letter);
+	}//end of method
+	
+	public void displayProgress() {
+		System.out.printf("Try to solve: %s%n", game.getCurrentProgress());
+	}//end of method
+	
+}//end of class
