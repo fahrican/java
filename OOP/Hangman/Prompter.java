f90ad22d5ba49a9564195f75b8b688a2e443c510
Fahri Can <fahri-can@gmx.at>
2016-12-17 17:06:05 +0100

    Update Prompter.java

--- OOP/Hangman/Prompter.java
+++ OOP/Hangman/Prompter.java
@@ -9,18 +9,54 @@ public class Prompter {
 		this.game = game;
 	}//end of constructor
 	
+	/*
+	 * checks if a letter fits the game name
+	 * @return true/false if the letter is correct
+	 */
 	public boolean promptForGuess() {
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter a letter: ");
-		String guess = scanner.nextLine();
-		char letter = guess.charAt(0);
+		boolean isHit = false;
+		boolean isAcceptable = false;
 		
-		return game.applyGuesses(letter);
+		do {
+			System.out.print("Enter a letter: ");
+			String guess = scanner.nextLine();
+			
+			try {
+				isHit = game.applyGuess(guess);
+			} catch (IllegalArgumentException iae) {
+				// TODO: handle exception
+				System.out.printf("%s. Please try again. %n", iae.getMessage());
+			}
+			
+		} while (isAcceptable);
+		
+		return isHit;
 	}//end of method
 	
+
+	// shows how many tries a player has left
 	public void displayProgress() {
-		System.out.printf("Try to solve: %s%n", game.getCurrentProgress());
+		System.out.printf("You have %d trie(s) left to solve: %s%n",
+							game.getRemainingTries(), game.getCurrentProgress());
 	}//end of method
+	
+	/*
+	 * end of the game it shows how many tries the player had left
+	 * otherwise it shows the name of the game
+	 */
+	public void displayOutcome() {
+		
+		if (game.isWon()) {
+			System.out.printf("Congratulation, you won with %d tries remaining.%n",
+								game.getRemainingTries());
+		}
+		else {
+			System.out.printf("Sorry, but you lost the game, the word was \"%s\"", 
+								game.getAnswer());
+		}
+		
+	}
 	
 }//end of class
