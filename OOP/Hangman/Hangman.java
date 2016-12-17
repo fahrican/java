8ddb30d4197eb8b2a36a82980146f58e95087e52
Fahri Can <fahri-can@gmx.at>
2016-12-17 17:04:55 +0100

    game update

--- OOP/Hangman/Hangman.java
+++ OOP/Hangman/Hangman.java
@@ -3,19 +3,16 @@ public class Hangman {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Game myGame = new Game("Monopoly");
 		Prompter prompter = new Prompter(myGame);
 		
-		prompter.displayProgress();
-		boolean isHit = prompter.promptForGuess();
-		if (isHit) {
-			System.out.println("You got a hit!");
+		while (myGame.getRemainingTries() > 0 && !myGame.isWon()) {
+			
+			prompter.displayProgress();
+			prompter.promptForGuess();
 		}
-		else {
-			System.out.println("Ooops, that was not correct.");
-		}
-		prompter.displayProgress();
+		prompter.displayOutcome();
 	}
 
 }
